1a8eb6883eb26376d8bec7ab02026cd95dae3bac
sergi <sergi.boquera@mecalux.com>
2024-09-26 16:09:08 +0200

    -savepoint before branch

--- Program.cs
+++ Program.cs
@@ -20,10 +20,17 @@ namespace NLiA
                 { 2, 1, 3, 1 },
                 { 8, 4, 5, 1 },
             };
+            double[,] r3 = new double[,] {
+                { 23, 18, -10, 4, -8 },
+                { 10, 28, -5, 2, -12 },
+                { 30, 45, -12, 6, -20 },
+                { -30, 18, 15, -3, -8 },
+                { 20, 54, -10, 4, -23 },
+            };
 
-            RREF reducer = new RREF(r2);
+            RREF reducer = new RREF(r3);
             reducer.process();
-            ElementaryOperations.print(r2);
+            ElementaryOperations.print(r3);
         }
     }
 }
